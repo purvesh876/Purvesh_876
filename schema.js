cc1a9652547eed7ff27c4e8be5d7eb2a654490e0
Purvesh Sakharkar <sakharkarpurvesh@gmail.com>
2025-08-07 01:24:22 +0530

    changes in schema.js joi

--- schema.js
+++ schema.js
@@ -1,26 +1,50 @@
+// const Joi = require('joi');
+
+
+// module.exports.listingSchema =Joi.object({
+
+// listing: Joi.object({
+
+// title: Joi.string().required(),
+
+// description: Joi.string().required(),
+
+// location: Joi.string().required(),
+
+// country: Joi.string().required(),
+
+// price: Joi.number().required().min(0),
+
+// image: Joi.string().allow("", null),
+
+// }).required(),
+
+// });
+
+
+// module.exports.reviewSchema = Joi.object({
+//   review: Joi.object({
+//     rating: Joi.number().required().min(1).max(5),
+//     comment: Joi.string().required(),
+//   }).required(),
+// });
 const Joi = require('joi');
 
+module.exports.listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().allow("", null),
 
-module.exports.listingSchema =Joi.object({
+    // ✅ Add this line for category validation
+    category: Joi.string().valid('Beach', 'Mountain', 'City', 'Forest', 'Desert', 'Lake', 'Other').required(),
 
-listing: Joi.object({
-
-title: Joi.string().required(),
-
-description: Joi.string().required(),
-
-location: Joi.string().required(),
-
-country: Joi.string().required(),
-
-price: Joi.number().required().min(0),
-
-image: Joi.string().allow("", null),
-
-}).required(),
-
+    
+  }).required(),
 });
-
 
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
