2f17dfce8a4ca6c44bfc4f7359a744e4fec76578
Purvesh Sakharkar <sakharkarpurvesh@gmail.com>
2025-08-08 16:08:44 +0530

    Error in route

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -1,16 +1,10 @@
-// const express = require("express");
-// const router = express.Router();
-// const { chatBotHandler } = require("../controllers/chatbot");
 
-// router.post("/", chatBotHandler);
-
-// module.exports = router;
 
 const express = require('express');
 const router = express.Router();
 const chatbotController = require('../controllers/chatbot');
 
-router.post('/', chatbotController.chatbot);
+router.post('/', chatbotController.chatBotHandler);
 
 module.exports = router;
 
